9e508de2bb00a64c5bdbb5788982ab74b42eab48
kiraaab <zS24013057@estudiantes.uv.mx>
2026-03-19 10:38:33 -0600

    Add infinite message sender function

---
+++ loop.js
@@ -0,0 +1,22 @@
+async function enviarInfinito(mensaje){
+	const textarea = document.querySelector('div[contenteditable="true"][role="textbox"]');
+
+	if(!textarea) throw new Error("No hay una conversación abierta");
+
+	while(true){
+		textarea.focus();
+		document.execCommand('insertText', false, mensaje);
+		textarea.dispatchEvent(new Event('input', {bubbles: true}));
+
+		const botonEnviar = document.querySelector('span[data-icon="wds-ic-send-filled"]')?.closest("div[role=button], button, div");
+		if(botonEnviar){
+			botonEnviar.click();
+		} else {
+			console.warn("No se encontró el botón de enviar");
+		}
+
+		await new Promise(resolve => setTimeout(resolve, 300));
+	}
+}
+
+enviarInfinito("mensaje de prueba");
